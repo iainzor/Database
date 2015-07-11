2c16bf1175e6820aa1bc102629555ffaac0cffba
Iain Edminster <iainedminster@gmail.com>
2015-07-10 17:20:26 -0700

    Added some more tests for the Registry

--- src/Tests/RegistryTest.php
+++ src/Tests/RegistryTest.php
@@ -27,4 +27,30 @@ class RegistryTest extends \PHPUnit_Framework_TestCase
 		
 		$this->assertEquals($testDb, $db);
 	}
+	
+	public function testConnectionFromConfig()
+	{
+		$registry = new Registry();
+		$registry->set("my-connection", [
+			Config::CONF_DSN => "mysql:host=127.0.0.1;dbname=bliss_database_tests",
+			Config::CONF_USER => "root"
+		]);
+		
+		$db = $registry->get("my-connection");
+		
+		$this->assertInstanceOf("\\Database\\PDO", $db);
+	}
+	
+	public function testDefaultConnectionFromConfig()
+	{
+		$registry = new Registry();
+		$registry->defaultConnection([
+			Config::CONF_DSN => "mysql:host=127.0.0.1;dbname=bliss_database_tests",
+			Config::CONF_USER => "root"
+		]);
+		
+		$db = $registry->defaultConnection();
+		
+		$this->assertInstanceOf("\\Database\\PDO", $db);
+	}
 }
