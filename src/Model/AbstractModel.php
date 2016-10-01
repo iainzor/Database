838eb29454dc0538425472b2506647aada82f54a
Iain Edminster <iainedminster@gmail.com>
2016-10-01 13:51:01 -0700

    Adding 'getSetDate' utility method to DB models

--- src/Model/AbstractModel.php
+++ src/Model/AbstractModel.php
@@ -4,4 +4,21 @@ namespace Database\Model;
 use Bliss\Component;
 
 abstract class AbstractModel extends Component implements ModelInterface
-{}
+{
+	/**
+	 * Utility method used to get or set date properties
+	 * 
+	 * @param string $field
+	 * @param mixed $value
+	 * @return int
+	 */
+	protected function getSetDate($field, $value)
+	{
+		if ($value !== null) {
+			if (!is_numeric($value)) {
+				$value = strtotime($value);
+			}
+		}
+		return $this->getSet($field, $value, self::VALUE_INT);
+	}
+}
